99ad7e2209ee565daa620dd6e5384adb551fa6de
Legendz253 <ijarvis@ltu.edu>
2021-12-07 10:03:37 -0500

    Game Manager stuff
    mostly from tutorial, replaced lives with bloodstain boolean.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,10 +5,70 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public GameObject[] ghosts;
-    public void GhostEaten(Ghost ghost) { 
-    
+    public Pacman pacman;
+    public Transform pellets;
+    bool bloodstain;
+    public int Score { get; private set; }
+
+    private void Start()
+    {
+        NewGame();
     }
-    public void PacmanEaten() { 
-    
+
+    private void NewGame()
+    {
+        SetScore(0);
+        NewRound();
+        bloodstain = false;
+    }
+
+    private void NewRound()
+    {
+        foreach (Transform pellet in this.pellets)
+        {
+            pellet.gameObject.SetActive(true);
+        }
+
+        ResetState();
+    }
+
+    private void ResetState()
+    {
+        for (int i = 0; i < this.ghosts.Length; i++)
+        {
+            this.ghosts[i].gameObject.SetActive(true);
+        }
+
+        this.pacman.gameObject.SetActive(true);
+    }
+
+    private void GameOver()
+    {
+        for (int i = 0; i < this.ghosts.Length; i++)
+        {
+            this.ghosts[i].gameObject.SetActive(false);
+        }
+
+        this.pacman.gameObject.SetActive(false);
+    }
+
+    public void GhostEaten(Ghost ghost) {
+        SetScore(this.Score + ghost.points);
+    }
+    public void PacmanEaten() {
+        this.pacman.gameObject.SetActive(false);
+        if (this.bloodstain == true)
+        {
+            GameOver();
+        }
+        else
+        {
+            bloodstain = true;
+            Invoke(nameof(ResetState), 3.0f);
+        }
+    }
+    private void SetScore(int Score)
+    {
+        this.Score = Score;
     }
 }
