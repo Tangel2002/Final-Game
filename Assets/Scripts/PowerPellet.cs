34ede42acd1766cd852d7f11aba85706b4d082f5
Legendz253 <ijarvis@ltu.edu>
2021-12-07 15:18:05 -0500

    power pellet?

--- Assets/Scripts/PowerPellet.cs
+++ Assets/Scripts/PowerPellet.cs
@@ -5,4 +5,9 @@ using UnityEngine;
 public class PowerPellet : Pellet
 {
     public float duration = 8.0f;
+
+    protected override void Eat()
+    {
+        FindObjectOfType<GameManager>().PowerPelletEaten(this);
+    }
 }
